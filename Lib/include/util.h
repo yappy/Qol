165fd7436c6ba90eee33bb7035a3cdc9a5232d7e
yappy <superunyo@yahoo.co.jp>
2015-12-31 04:08:43 +0900

    Add doc

--- Lib/include/util.h
+++ Lib/include/util.h
@@ -1,3 +1,7 @@
+/** @file
+ * @brief Utilities.
+ */
+
 #pragma once
 
 #include <memory>
@@ -7,8 +11,16 @@
 #include <Unknwn.h>
 
 namespace yappy {
+/// Utilities.
 namespace util {
 
+/** @brief Noncopyable class.
+ * @details Delete copy constructor and assignment operator.
+ * @code
+ * // How to use:
+ * class MyClass : private noncopyable { ... };
+ * @endcode
+ */
 class noncopyable {
 public:
 	noncopyable() = default;
@@ -17,8 +29,13 @@ public:
 };
 
 
+/** @brief Fixed size char array for string ID.
+ * @see std::hash<yappy::util::IdString>
+ */
 using IdString = std::array<char, 16>;
 
+/** @brief Create yappy::util::IdString from C-style string.
+ */
 template <size_t N>
 inline void createFixedString(std::array<char, N> *out, const char *src) {
 	size_t len = std::strlen(src);
@@ -32,6 +49,7 @@ inline void createFixedString(std::array<char, N> *out, const char *src) {
 }
 
 
+/// Deleter: auto CloseHandle().
 struct handleDeleter {
 	using pointer = HANDLE;
 	void operator()(HANDLE h)
@@ -41,25 +59,35 @@ struct handleDeleter {
 		}
 	}
 };
+/// unique_ptr of HANDLE with handleDeleter.
 using HandlePtr = std::unique_ptr<HANDLE, handleDeleter>;
 
+/// Deleter: auto IUnknown::Release().
 inline void iunknownDeleter(IUnknown *iu)
 {
 	iu->Release();
 }
-typedef decltype(&iunknownDeleter) IUnknownDeleterType;
+using IUnknownDeleterType = decltype(&iunknownDeleter);
+/// unique_ptr of IUnknown with iunknownDeleter().
 template<class T>
 using IUnknownPtr = std::unique_ptr<T, IUnknownDeleterType>;
+/// shared_ptr of IUnknown. Deleter must be specified at construction.
 template<class T>
 using IUnknownSharedPtr = std::shared_ptr<T>;
 
+/// Deleter: auto flose().
 inline void fileDeleter(FILE *fp)
 {
 	::fclose(fp);
 }
+/// unique_ptr of FILE with fileDeleter().
 using FilePtr = std::unique_ptr<FILE, decltype(&fileDeleter)>;
 
 
+/** @brief Wide char to UTF-8.
+ * @param[in] in	Wide string.
+ * @return UTF-8 string.
+ */
 inline std::unique_ptr<char[]> wc2utf8(const wchar_t *in)
 {
 	int len = ::WideCharToMultiByte(CP_UTF8, 0, in, -1, nullptr, 0, nullptr, nullptr);
@@ -68,6 +96,10 @@ inline std::unique_ptr<char[]> wc2utf8(const wchar_t *in)
 	return pBuf;
 }
 
+/** @brief UTF-8 to wide char.
+ * @param[in] in	UTF-8 string.
+ * @return Wide char string.
+ */
 inline std::unique_ptr<wchar_t[]> utf82wc(const char *in)
 {
 	int len = ::MultiByteToWideChar(CP_UTF8, 0, in, -1, nullptr, 0);
@@ -76,17 +108,23 @@ inline std::unique_ptr<wchar_t[]> utf82wc(const char *in)
 	return pBuf;
 }
 
+/** @brief Auto CoInitializeEx() and CoUninitialize() class.
+*/
 class Com : private noncopyable {
 public:
+	/// CoInitializeEx(nullptr, COINIT_MULTITHREADED)
 	Com() { ::CoInitializeEx(nullptr, COINIT_MULTITHREADED); }
+	/// CoUninitialize()
 	~Com() { ::CoUninitialize(); }
 };
 
-}
-}
+}	// namespace util
+}	// namespace yappy
 
 namespace std {
 
+/** @brief Hash function object of yappy::util::IdString for std::unordered_map.
+ */
 template <>
 struct hash<yappy::util::IdString> {
 	size_t operator()(const yappy::util::IdString &key) const
@@ -100,5 +138,4 @@ struct hash<yappy::util::IdString> {
 	}
 };
 
-}
-
+}	// namespace std
